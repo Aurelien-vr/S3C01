421d081394b4bab772e304e785bdcd4b7d7db691
AURELIEN VINCENT--RANDONNIER <80596073+Aurelien-vr@users.noreply.github.com>
2024-12-16 02:28:21 +0100

    Update testRunner.java

--- S3C01_Project/src/test/testRunner.java
+++ S3C01_Project/src/test/testRunner.java
@@ -10,16 +10,16 @@ import org.junit.runners.Suite;
 	test_avis_taxe_fonciere.class,
 	test_bien.class,
 	test_contrat_colocation.class,
-    test_contrat_location.class,
-    test_declaration_revenu.class,
-    test_etat_des_lieux.class,
-    test_facture_eau.class,
-    test_facture_electricite.class,
-    test_facture_gaz.class,
-    test_facture.class,
-    test_locataire.class,
-    test_regularisation_charges.class,
-    test_solde_de_tout_compte.class,
-    test_travaux.class
+	test_contrat_location.class,
+    	test_declaration_revenu.class,
+	test_etat_des_lieux.class,
+	test_facture_eau.class,
+	test_facture_electricite.class,
+	test_facture_gaz.class,
+	test_facture.class,
+	test_locataire.class,
+	test_regularisation_charges.class,
+	test_solde_de_tout_compte.class,
+	test_travaux.class
 })
 public class testRunner {}
